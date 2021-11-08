812e092f15abbadedc500cb48ed04e2b81e00b35
Kash Pitan <kash.pitan@igloo.energym>
2021-11-08 12:51:12 +0000

    React router set up

---
+++ client/src/screens/chatScreen.tsx
@@ -0,0 +1,90 @@
+import React, { FC, useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+import axios from 'axios';
+import { io } from "socket.io-client";
+
+// import '../App.css';
+import "materialize-css/dist/css/materialize.min.css";
+
+import MessageList from '../components/MessageList';
+
+
+const ChatScreen: FC = (): JSX.Element => {
+  const [messages, setMessages] = useState<string[]>([]);
+  const [messageInput, setMessageInput] = useState<string>('');
+  const [usernameState, setUsernameState] = useState<string>('username not set');
+
+  const { username } = useParams();
+
+  useEffect(() => {
+    setUsernameState(username ? username : 'username not set');
+   const socket = io('localhost:4000',{
+    withCredentials: true,
+  });
+   socket.on('message', (data: string) => {
+     console.log(data);
+     setMessages((prevState) => [...prevState, data]);
+   })
+
+    //helps prevent duplicate websocket events by closing websocket when done
+    return () => {
+      socket.close();
+    }
+  }, []);
+
+  const sendButtonHandler = async (e: any) => {
+    e.preventDefault();
+    e.stopPropagation();
+    try {
+      await axios.post('http://localhost:4000/chat/message',
+        {
+          message: messageInput
+        }, 
+        {
+          headers: {
+            'content-type': 'application/json'
+          }
+        });
+    } catch (error) {
+      console.log('error ==> ', error);
+    }
+    setMessageInput('');
+  }
+
+  const messageBoxChangeHandler = (e: any) => {
+    setMessageInput(e.target.value);
+  };
+
+  return (
+    <>
+      <div className="row">
+        <h1>{usernameState}</h1>
+        <MessageList messages={messages}></MessageList>
+        {/* {messages.map((message: string) => {
+          return <h1>{message}</h1>
+        })} */}
+
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s12">
+              <textarea
+                id='messageInput'
+                className="materialize-textarea"
+                placeholder='enter message'
+                value={messageInput}
+                onChange={messageBoxChangeHandler}
+                ></textarea>
+            </div>
+          </div>
+
+          <button className="btn waves-effect waves-light" type="submit" name="action" onSubmit={sendButtonHandler} onClick={sendButtonHandler}>Send
+            <i className="material-icons right">send</i>
+          </button>
+
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default ChatScreen;
--- client/src/screens/usernameInputScreen.tsx
+++ client/src/screens/usernameInputScreen.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { FC, useState } from 'react'
+import { useNavigate } from "react-router-dom";
 
- const usernameInputScreen = () => {
+
+ const UsernameInputScreen: FC = (): JSX.Element => {
+   const [usernameInput, setUsernameInput] = useState<string>('');
+   const navigate = useNavigate();
+
+   const usernameInputChangeHandler = (e: any) => {
+      setUsernameInput(e.target.value)
+   };
+
+   const enterChatButtonHandler = async () => {
+      navigate(`/chat/${usernameInput}`);
+   }
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <form className="col s12">
+        <div className="row">
+          <div className="input-field col s12">
+            <textarea
+              id='usernameInput'
+              className="materialize-textarea"
+              placeholder='enter username'
+              value={usernameInput}
+              onChange={usernameInputChangeHandler}
+              ></textarea>
+          </div>
+        </div>
+
+        <button 
+          className="btn waves-effect waves-light"
+          type="submit"
+          name="action"
+          onSubmit={enterChatButtonHandler}
+          onClick={enterChatButtonHandler}>
+            Send
+          <i className="material-icons right">Enter Chat</i>
+        </button>
+      </form>
+    </>
   )
 }
 
-export default usernameInputScreen;
+export default UsernameInputScreen;
