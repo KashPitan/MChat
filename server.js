03db97022cc0787fcfaaafb9e15de7133ff64cfa
Kash Pitan <kashpitan@googlemail.com>
2021-11-26 18:02:41 +0000

    server changes to serve react files

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const Socket = require("socket.io");
-const cors = require('cors')
+const cors = require("cors");
 const mongoose = require("mongoose");
 
-const Chat = require('./schema/chatSchema');
+const Chat = require("./schema/chatSchema");
 // const Message = require('./schema/messageSchema');
 
-const chatRoutes = require('./routes/chatRoutes');
-
+const chatRoutes = require("./routes/chatRoutes");
 
 dotenv.config();
 // console.log(process.env.PORT);
@@ -19,7 +18,15 @@ const URI = process.env.MONGO_URI;
 const App = express();
 
 App.use(cors());
-App.use(express.json())
+App.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  App.use(express.static("client/build"));
+
+  App.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+  });
+}
 
 const server = App.listen(process.env.PORT || 4000, () => {
   if (process.env.PORT) {
@@ -29,22 +36,22 @@ const server = App.listen(process.env.PORT || 4000, () => {
   }
 });
 
-App.use('/chat', chatRoutes);
+App.use("/chat", chatRoutes);
 
 var IO = Socket(server, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
-    credentials: true
-  }
+    credentials: true,
+  },
 });
 
 IO.on("connection", (socket) => {
   console.log(`socket connection made ${socket.id}`);
 
-  IO.on('disconnect', ()=> {
-    console.log(`disconnected ${socket.id}`)
-  })
+  IO.on("disconnect", () => {
+    console.log(`disconnected ${socket.id}`);
+  });
 });
 
 const MongoOptions = {
@@ -52,37 +59,36 @@ const MongoOptions = {
   useNewUrlParser: true,
 };
 
-mongoose
-  .connect(URI, MongoOptions)
-  .catch((err) => console.log(err));
+mongoose.connect(URI, MongoOptions).catch((err) => console.log(err));
 
 const connection = mongoose.connection;
 
-connection.once('open', async ()=> {
-  console.log('connected to db');
+connection.once("open", async () => {
+  console.log("connected to db");
 
-  const filter = [{
-    $match: {
+  const filter = [
+    {
+      $match: {
         $and: [
-            { "updateDescription.updatedFields": { $exists: true } },
-            { operationType: "update" }]
-          }
-      }];
+          { "updateDescription.updatedFields": { $exists: true } },
+          { operationType: "update" },
+        ],
+      },
+    },
+  ];
 
-  Chat.watch(filter, {fullDocument: 'updateLookup'}).
-    on('change', (data) => {
+  Chat.watch(filter, { fullDocument: "updateLookup" }).on("change", (data) => {
+    const lengthOfMessageArray = data.fullDocument.messages.length;
+    const messageArray = data.fullDocument.messages[lengthOfMessageArray - 1];
+    console.log("data.fullDocument.messages ==> ", messageArray);
+    console.log(new Date(), data.updateDescription.updatedFields);
 
-      const lengthOfMessageArray = data.fullDocument.messages.length;
-      const messageArray = data.fullDocument.messages[lengthOfMessageArray - 1];
-      console.log('data.fullDocument.messages ==> ', messageArray);
-      console.log(new Date(), data.updateDescription.updatedFields);
-
-      IO.emit('message', messageArray);
-    });
+    IO.emit("message", messageArray);
+  });
 
   // Insert a doc, will trigger the change stream handler above
   // console.log(new Date(), 'Inserting doc');
   // await Chat.create({ name: 'Main' });
-})
+});
 
 module.exports = App;
