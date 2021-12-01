8c175c1fb5cb206541a435ad7222f223c01b074d
Kash Pitan <kashpitan@googlemail.com>
2021-12-01 00:42:14 +0000

    adjusted urls again? im tired man

--- client/src/screens/chatScreen.tsx
+++ client/src/screens/chatScreen.tsx
@@ -1,55 +1,62 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 import { io } from "socket.io-client";
 
 // import '../App.css';
 import "materialize-css/dist/css/materialize.min.css";
 
-import MessageList from '../components/MessageList';
-
+import MessageList from "../components/MessageList";
 
 const ChatScreen: FC = (): JSX.Element => {
   const [messages, setMessages] = useState<string[]>([]);
-  const [messageInput, setMessageInput] = useState<string>('');
-  const [usernameState, setUsernameState] = useState<string>('username not set');
+  const [messageInput, setMessageInput] = useState<string>("");
+  const [usernameState, setUsernameState] =
+    useState<string>("username not set");
 
   const { username } = useParams();
+  const requestAddressBaseUrl =
+    process.env.NODE_ENV === "production"
+      ? "http://localhost:4000"
+      : "https://mstream-chat-dev.herokuapp.com";
 
   useEffect(() => {
-    setUsernameState(username ? username : 'username not set');
-   const socket = io('localhost:4000',{
-    withCredentials: true,
-  });
-   socket.on('message', (data: string) => {
-     console.log(data);
-     setMessages((prevState) => [...prevState, data]);
-   })
+    setUsernameState(username ? username : "username not set");
+    const socket = io(requestAddressBaseUrl, {
+      withCredentials: true,
+    });
+    socket.on("message", (data: string) => {
+      console.log(data);
+      setMessages((prevState) => [...prevState, data]);
+    });
 
     //helps prevent duplicate websocket events by closing websocket when done
     return () => {
       socket.close();
-    }
+    };
   }, []);
 
   const sendButtonHandler = async (e: any) => {
     e.preventDefault();
     e.stopPropagation();
+
     try {
-      await axios.post('http://localhost:4000/chat/message',
+      await axios.post(
+        requestAddressBaseUrl + "/chat/message",
         {
-          message: messageInput
-        }, 
+          message: messageInput,
+        },
         {
           headers: {
-            'content-type': 'application/json'
-          }
-        });
+            "content-type": "application/json",
+          },
+        }
+      );
     } catch (error) {
-      console.log('error ==> ', error);
+      console.log("error ==> ", error);
     }
-    setMessageInput('');
-  }
+    setMessageInput("");
+  };
 
   const messageBoxChangeHandler = (e: any) => {
     setMessageInput(e.target.value);
@@ -68,23 +75,29 @@ const ChatScreen: FC = (): JSX.Element => {
           <div className="row">
             <div className="input-field col s12">
               <textarea
-                id='messageInput'
+                id="messageInput"
                 className="materialize-textarea"
-                placeholder='enter message'
+                placeholder="enter message"
                 value={messageInput}
                 onChange={messageBoxChangeHandler}
-                ></textarea>
+              ></textarea>
             </div>
           </div>
 
-          <button className="btn waves-effect waves-light" type="submit" name="action" onSubmit={sendButtonHandler} onClick={sendButtonHandler}>Send
+          <button
+            className="btn waves-effect waves-light"
+            type="submit"
+            name="action"
+            onSubmit={sendButtonHandler}
+            onClick={sendButtonHandler}
+          >
+            Send
             <i className="material-icons right">send</i>
           </button>
-
         </form>
       </div>
     </>
   );
-}
+};
 
 export default ChatScreen;
